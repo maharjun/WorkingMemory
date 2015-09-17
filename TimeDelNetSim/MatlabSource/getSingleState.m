f1f110b1aa3bc06b79a49df261bbb4ce830059c3
maharjun <arjun210493@gmail.com>
2015-09-17 18:07:48 +0530

    Made getSingleState work for general MATLAB Struct
    The changes in this commit are:
    
       1. Altered getSingleState and introduced a new function
          getSingleRecord in order to be able to extract the
    	  state for a single time step irrespective of the
    	  variables in the struct.
    
       2. Using the new MexMemoryInterfacing revision with the
          changed MexoryCounter rules. (not of much use for the
    	  current revision)
    
    Library Versions -
    
       MexMemoryInterfacing - 96b830b Changed Memory usage checking rule for reserve and trim.
       RandomNumGen         - 88e4b6c Added Submodule and tbb support for project

--- TimeDelNetSim/MatlabSource/getSingleState.m
+++ TimeDelNetSim/MatlabSource/getSingleState.m
@@ -2,37 +2,14 @@ function [ SingleState ] = getSingleState( StateStruct, timeInstant )
 % CONVERTSTATETOINITIALCOND Converts structs
 %   basically a name conversion Function as such. 
 
-timeDimensionLength = length(StateStruct.Time);
-timeIndex = 1;
-if timeDimensionLength == 1
-	timeIndex = 1;
-elseif nargin() == 2 
-	timeIndex = find(StateStruct.Time == timeInstant, 1);
-	if isempty(timeIndex)
-		Ex = MException('NeuralSim:ConvertStatetoInitialCond:InvalidTimeInstant', ...
-					'Need to specify a time instant belonging to from StateStruct.Time');
-		throw(Ex);
-	end
-
+timeIndex = find(StateStruct.Time == timeInstant, 1);
+if isempty(timeIndex)
+	Ex = MException('NeuralSim:ConvertStatetoInitialCond:InvalidTimeInstant', ...
+				'Need to specify a time instant belonging to from StateStruct.Time');
+	throw(Ex);
 end
 
-SingleState.V = StateStruct.V(:, timeIndex);
-SingleState.U = StateStruct.U(:, timeIndex);
-SingleState.Iin1 = StateStruct.Iin1(:, timeIndex);
-SingleState.Iin2 = StateStruct.Iin2(:, timeIndex);
-SingleState.Iext.Irand = StateStruct.Iext.Irand(:, timeIndex);
-SingleState.Iext.GenState = StateStruct.Iext.GenState(:, timeIndex);
-SingleState.Time = StateStruct.Time(timeIndex);
-
-SingleState.CurrentQIndex = StateStruct.CurrentQIndex(timeIndex);
-if timeDimensionLength == 1
-	SingleState.SpikeQueue = StateStruct.SpikeQueue;
-else
-	SingleState.SpikeQueue = StateStruct.SpikeQueue{timeIndex};
-end
-
-SingleState.LSTNeuron = StateStruct.LSTNeuron(:, timeIndex);
-SingleState.LSTSyn = StateStruct.LSTSyn(:, timeIndex);
+SingleState = getSingleRecord(StateStruct, timeIndex);
 
 end
 
